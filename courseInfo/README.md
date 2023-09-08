3b5ecd9652df1f02f8ba5484071fbe9d5e400750
pipiolus <giovanipaolonii@gmail.com>
2023-09-08 17:03:37 -0300

    Finished part 2 exercises: 2.1 to 2.5 included (courseInfo step6 to step10)

---
+++ courseInfo/README.md
@@ -0,0 +1,3 @@
+courseInfo - PART 2
+
+I wanted to do every exercise in the most real way as possible, frustating myself and developing my own ways to improve my skills as a web dev in this beautyful journey, so I've used my own code from exercises "PART 1" to refactor my own app. Hope this wasnt a problem.
---
+++ courseInfo/src/components/Course.jsx
@@ -0,0 +1,52 @@
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      <strong>
+        {name} {exercises}
+      </strong>
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => (sum += part.exercises), 0);
+
+  return (
+    <div>
+      <p>
+        <strong>Total of {total} exercises</strong>
+      </p>
+    </div>
+  );
+};
+
+const Course = (props) => {
+  const { course } = props;
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
