7d0d26187e84be49e581937f0ad909aff38edace
pipiolus <giovanipaolonii@gmail.com>
2023-09-13 12:21:58 -0300

    Initial state of App stored in db.json. Started json-server (phonebook step6)

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Person from "./components/Person";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
-function App(props) {
-  const [persons, setPersons] = useState(props.persons);
+function App() {
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+      });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
