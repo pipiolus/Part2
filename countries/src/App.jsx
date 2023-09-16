a168064bf96320d38dafe83c3e36ecb96b76a4a8
pipiolus <giovanipaolonii@gmail.com>
2023-09-16 18:15:51 -0300

    some improvements

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,6 +1,21 @@
 import Search from "./components/Search";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  axios
+    .get(
+      "https://studies.cs.helsinki.fi/restcountries/api/name/Argentina"
+    )
+    .then((response) => {
+      const country = response.data;
+      console.log(
+        country.name.common,
+        country.capital[0],
+        country.languages
+      );
+    });
+
   return (
     <>
       <div>
