21f80795f9d38adca06c131a8cb40aa0a653a521
pipiolus <giovanipaolonii@gmail.com>
2023-09-18 23:03:40 -0300

    Finished countries step1

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,27 +1,42 @@
-import Search from "./components/Search";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Search from "./components/Search";
+import Country from "./components/Country";
 
 function App() {
   const [value, setValue] = useState("");
+  const [all, setAll] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then((response) => console.log(response.data));
-  });
+      .then((response) => response.data)
+      .then((allCountries) => {
+        setAll(allCountries);
+      });
+  }, []);
 
   const handleChange = (e) => {
     setValue(e.target.value);
   };
 
+  const filterArray = (arr, str) => {
+    return arr.filter((obj) =>
+      obj.name.common.toLowerCase().match(str.toLowerCase())
+    );
+  };
+
+  console.log(filterArray(all, value));
   return (
-    <>
+    <div>
+      <h1>Data For Countries</h1>
       <div>
-        <h1>Data For Countries</h1>
         <Search value={value} handleValue={handleChange} />
       </div>
-    </>
+      <div>
+        <Country countries={filterArray(all, value)} />
+      </div>
+    </div>
   );
 }
 
---
+++ countries/src/components/Country.jsx
@@ -0,0 +1,46 @@
+const Country = ({ countries }) => {
+  if (countries.length === 0 || countries.length === 250) return null;
+
+  if (countries.length >= 10) {
+    return (
+      <div>
+        <h4>Too many matches. Try being more specific...</h4>
+      </div>
+    );
+  } else if (countries.length > 1 && countries.length < 10) {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.idd.suffixes[0]}>
+            <b>{country.name.common}</b>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    const country = countries[0];
+    const languages = Object.values(country.languages);
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <div>
+          <p>Capital: {country.capital}</p>
+          <p>Area: {country.area} km²</p>
+        </div>
+        <div>
+          <h3>Languages:</h3>
+          <ul>
+            {languages.map((lang, index) => (
+              <li key={index}>{lang}</li>
+            ))}
+          </ul>
+          <div>
+            <h3>Flag:</h3>
+            <div className="flag">{country.flag}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+export default Country;
