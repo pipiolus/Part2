aa527d9be827f64a7f9cff19fdf8316386e3c76c
pipiolus <giovanipaolonii@gmail.com>
2023-09-19 17:43:03 -0300

    Deleted all code to restart the app. It got realy hard at this moment

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,42 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Search from "./components/Search";
-import Country from "./components/Country";
 
 function App() {
-  const [value, setValue] = useState("");
-  const [all, setAll] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
-      .then((response) => response.data)
-      .then((allCountries) => {
-        setAll(allCountries);
-      });
-  }, []);
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
-  const filterArray = (arr, str) => {
-    return arr.filter(
-      (obj) =>
-        obj.name.common.toLowerCase().match(str.toLowerCase()) ||
-        obj.name.official.toLowerCase().match(str.toLowerCase())
-    );
-  };
+  const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [countryDetails, setCountryDetails] = useState(null);
 
   return (
     <div>
       <h1>Data For Countries</h1>
-      <div>
-        <Search value={value} handleValue={handleChange} />
-      </div>
-      <div>
-        <Country countries={filterArray(all, value)} />
-      </div>
     </div>
   );
 }
